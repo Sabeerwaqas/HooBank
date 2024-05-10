1ef4198149f91a0d9f916599aa06e34d16dd029d
Sabeerwaqas <sabeerwaqas050@gmail.com>
2024-05-10 11:05:50 -0700

    [Add: Download project images and make file]

---
+++ src/assets/index.js
@@ -0,0 +1,68 @@
+const projectImages = {
+
+    //=====> Images use in navbar and footer start
+
+    logo: require("./images/logo.png"),
+
+    //=====> Images use in navbar and footer end
+
+    //=====> Images use in first section start
+
+    robotHand: require("./images/robotHand.png"),
+    cardOneImg: require("./images/cardOne.png"),
+    cardTwoImg: require("./images/cardTwo.png"),
+    cardThreeImg: require("./images/cardThree.png"),
+    discountImg: require("./images/"),
+
+    //=====> Images use in first section end
+
+    //=====> Images use in second section start
+
+    rewards: require("./images/rewards.png"),
+    secured: require("./images/secured.png"),
+    balanceTransfer: require("./images/balanceTransfer.png"),
+
+    //=====> Images use in second section end
+
+    //=====> Images use in third section start
+
+    dribblePro: require("./images/dribblePro.png"),
+    netflix: require("./images/netflix.png"),
+    parallelImg: require("./images/parallelImage.png"),
+    paypalImg: require("./images/paypal.png"),
+    appStore: require("./images/appStore.png"),
+    googlePlay: require("./images/googlePlay.png"),
+
+    //=====> Images use in third section end
+
+    //=====> Images use in fourth section start
+
+    appleLogo: require("./images/apple.png"),
+    visaLogo: require("./images/visa.png"),
+    shopifyLogo: require("./images/shopify.png"),
+    chartLine: require("./images/chartLine.png"),
+
+    //=====> Images use fourth section end
+
+    //=====> Images use fifth section start
+    
+    cardParallelImg: require("./images/reviewCardParallelImg.png"),
+    personOne: require("./images/personOne.png"),
+    personTwo: require("./images/personTwo.png"),
+    personThree: require("./images/personThree.png"),
+
+    //=====> Images use fifth section end
+
+    //=====> Images use sixth section start
+
+    airBnb:require("./images/airbnb.png"),
+    binance:require("./images/binance.png"),
+    coinbase:require("./images/coinbase.png"),
+    dropbox:require("./images/dropbox.png")
+
+    //=====> Images use sixth section end
+
+
+}
+
+export default projectImages;
